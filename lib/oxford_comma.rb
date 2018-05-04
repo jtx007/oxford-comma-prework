87cf1b303e05a68dd453d0e45d9cdedef056f1f4
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-04 17:06:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,8 @@ def oxford_comma(array)
 elsif array.length == 2
   array.join(" and ")
   elsif array.length == 3
-  array.join(", ")
   array.insert(2, "and")
-  array.join
+  array.join(", ")
   
   
 end
