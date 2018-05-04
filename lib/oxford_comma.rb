523e4f10485a22d22b8aebe81c06d51c017bfe10
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-04 17:19:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,15 +3,10 @@ def oxford_comma(array)
   array.join
 elsif array.length == 2
   array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
   last =  array.pop
   array << "and #{last}"
   array.join(", ")
-elsif array.length > 3
-  last = array.pop
-  array << "and #{last}"
-  array.join(", ")
-  
   
 end
 end
